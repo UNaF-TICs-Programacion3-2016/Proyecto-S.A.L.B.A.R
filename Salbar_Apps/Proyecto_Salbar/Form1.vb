e2ed0729ad1c53f72ced81f580e009cfbbbb61da
SnGa089 <SnGa0089@gmail.com>
2016-11-11 12:41:58 -0300

    Incorporacion de la clase "Frecuencia"

--- Salbar_Apps/Proyecto_Salbar/Form1.vb
+++ Salbar_Apps/Proyecto_Salbar/Form1.vb
@@ -3,4 +3,9 @@
         MsgBox("probando la sincronizacion con el repositorio")
         MsgBox("pueba exitosa")
     End Sub
+
+    '' Suponiendo que ya tenemos la matriz donde se aloja todos los valores para cada milisegundo
+    Public Function Comparacion_de_frecuencias()
+        Frecuencia.
+    End Function
 End Class
