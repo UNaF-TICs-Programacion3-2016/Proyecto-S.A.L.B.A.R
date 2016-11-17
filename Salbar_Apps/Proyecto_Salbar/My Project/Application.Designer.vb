1e5ce66d1980f8cfd1b8c7b00dc0d2d179955318
SnGa089 <SnGa0089@gmail.com>
2016-11-16 21:28:56 -0300

    Super Actualizacion Clase Conexion v10.8.1

--- Salbar_Apps/Proyecto_Salbar/My Project/Application.Designer.vb
+++ Salbar_Apps/Proyecto_Salbar/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proyecto_Salbar.Form1
+            Me.MainForm = Global.Proyecto_Salbar.Administrador
         End Sub
     End Class
 End Namespace
